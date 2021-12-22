51ba6f68b334cb0e38e7b6124a715a78ee367205
tomoya_edzuka <80945931+t-edzuka@users.noreply.github.com>
2021-12-22 20:16:53 +0900

    app setting

---
+++ app/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI, Depends
+
+from app.config import get_settings, Settings
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def root():
+    return {'root': 'Hello'}
+
+
+@app.get("/ping")
+def pong(settings: Settings = Depends(get_settings)):
+    return {"ping": "pong!",
+            'environment': settings.environment,
+            'testing': settings.testing,
+            }
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
